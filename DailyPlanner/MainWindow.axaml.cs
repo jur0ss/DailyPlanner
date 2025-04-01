16b2fbced56865d61a14485dedb247140f06ef13
jurekg <jurek.gryz2007@gmail.com>
2025-04-01 03:09:15 +0200

    Added textblock to display added tasks

--- DailyPlanner/MainWindow.axaml.cs
+++ DailyPlanner/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace DailyPlanner;
 
@@ -7,5 +8,36 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        SubmitButton.Click += SubmitButton_Click
     }
+
+    private void SubmitButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var taskName = TextBoxTaskName.Text;
+            
+            var category = (CategoryCombo.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nie wybrano odpowiedzi";
+        }
+    }
+    
+    public class Zadanie
+    {
+        public string Nazwa { get; set; }
+        public string Kategoria { get; set; }
+        public bool CzyUkonczone { get; set; }
+        
+        public Zadanie(string nazwa, string kategoria, bool czyUkonczone)
+        {
+            Nazwa = nazwa;
+            Kategoria = kategoria;
+            CzyUkonczone = czyUkonczone;
+        }
+    
+    
+    
+    
+    
+    
 }
