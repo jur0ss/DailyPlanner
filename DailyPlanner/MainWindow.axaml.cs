99d3f82e5e8ba234aa1c8d25d7f07d2731d1abfc
jurekg1 <jurek@poczta.pl>
2025-04-28 10:24:35 +0200

    Working on submit button click

--- DailyPlanner/MainWindow.axaml.cs
+++ DailyPlanner/MainWindow.axaml.cs
@@ -8,8 +8,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
-        SubmitButton.Click += SubmitButton_Click
+
+        SubmitButton.Click += SubmitButton_Click;
     }
 
     private void SubmitButton_Click(object sender, RoutedEventArgs e)
@@ -17,27 +17,31 @@ public partial class MainWindow : Window
         try
         {
             var taskName = TextBoxTaskName.Text;
-            
-            var category = (CategoryCombo.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nie wybrano odpowiedzi";
+
+            var category = (CategoryCombo.SelectedItem as ComboBoxItem)?.Content?.ToString() ??
+                           "Nie wybrano odpowiedzi";
         }
+        catch{}
     }
-    
+
+
     public class Zadanie
     {
         public string Nazwa { get; set; }
         public string Kategoria { get; set; }
         public bool CzyUkonczone { get; set; }
-        
+
         public Zadanie(string nazwa, string kategoria, bool czyUkonczone)
         {
             Nazwa = nazwa;
             Kategoria = kategoria;
             CzyUkonczone = czyUkonczone;
         }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+    }
 }
